3875891c4a09ff16ed4f372ed68e4b4294d73c9e
AlexWeinreb <alexis.weinreb@yale.edu>
2022-07-06 15:44:13 -0400

    Results analysis.

--- count_stops_by_shift.R
+++ count_stops_by_shift.R
@@ -35,13 +35,11 @@ count_stops <- function(seq, shift){
 nb_of_stop_codons <- function(seq){
   gene_length <- nchar(seq)
   
-  if(count_stops(seq,0) != 1) warning("Incorrect number of codons without shift.")
-  
+  shift_0 <- count_stops(seq,0)
   shift_1 <- count_stops(seq,1)
-  
   shift_2 <- count_stops(seq,2)
   
-  data.frame(gene_length, shift_1, shift_2)
+  data.frame(gene_length, shift_0, shift_1, shift_2)
 }
 
 
@@ -57,14 +55,16 @@ any(is.na(seqs))
 # tx_id[is.na(seqs)]
 # seqs <- seqs[!is.na(seqs)]
 
-seqs_as_bs <- seqs |>
-  lapply(Biostrings::DNAString)
-
-nb_codons <- seqs_as_bs |>
-  lapply(nb_of_stop_codons)
-
-shifts <- do.call(what = rbind, args = nb_codons)
+shifts <- seqs |>
+  lapply(nb_of_stop_codons) |>
+  do.call(what = rbind, args = _)
 shifts$tx_id <- tx_id
+shifts$gene_id <- wb_tx2g(shifts$tx_id, tx2g_tab = tx2g, warn_missing = TRUE)
+
+# Should all have a single stop codon without frameshift
+# ctb-1/MTCE.21 weird, there may be others
+shifts[shifts$shift_0 != 1,]
+shifts <- shifts[shifts$shift_0 == 1,]
 
 
 library(ggplot2)
@@ -74,18 +74,47 @@ ggplot(shifts) + theme_classic() +
   geom_point(aes(shift_1, shift_2)) +
   geom_abline(slope = 1, color = "grey") +
   coord_equal() +
-  ggrepel::geom_text_repel(aes(shift_1, shift_2, label = tx_id)) +
+  # ggrepel::geom_text_repel(aes(shift_1, shift_2, label = tx_id)) +
   scale_x_log10() + scale_y_log10()
 
-hist(shifts$shift_2/shifts$shift_1, breaks = 100)
+ggplot(shifts) +theme_classic() +
+  geom_histogram(aes(x = shift_2/shift_1), bins = 100, color = "white") +
+  scale_x_log10()
+
+hist(log(shifts$shift_2/shifts$shift_1), breaks = 100)
+
+# there is a systematic bias: frameshifts of 2  bp tend to induce more stop codons
+# than frameshifts of 1 bp.
+# However, not clearly mutually exclusive: in general genes with more of 1 also 
+# have more of the other (presumably because longer, see below), but the initial hypothesis
+# that 'some genes "prefer" a 1 bp shift whereas others "prefer" 2 bp' is not correct.
+
+ggplot(shifts) + theme_classic() +
+  geom_point(aes(gene_length, shift_2)) +
+  # ggrepel::geom_text_repel(aes(shift_1, shift_2, label = tx_id)) +
+  scale_x_log10() + scale_y_log10()
+
+ggplot(shifts) + theme_classic() +
+  geom_point(aes(gene_length, shift_1)) +
+  # ggrepel::geom_text_repel(aes(shift_1, shift_2, label = tx_id)) +
+  scale_x_log10() + scale_y_log10()
+
+
+mod <- lm(shift_2 ~ shift_1 + gene_length, data = shifts)
+
+plot(fitted.values(mod), residuals(mod))
+plot(log(fitted.values(mod)), residuals(mod))
+plot(log(fitted.values(mod)), log(residuals(mod)), xlim=c(-2,6))
+mod
+summary(mod)
+
+ggplot(shifts) + theme_classic() +
+  geom_point(aes(x = shift_1/gene_length, y = shift_2/gene_length),
+             alpha = .2) +
+  geom_abline(slope = 1, color = "grey")
+
+# Once removing the influence of gene_length, this relationship is even clearer
 
 
 
-# xx2 <- bench::press(n = seq(from = 1, to = 25, by = 5),
-#                     bench::mark(a = seqs_as_bs[sample(500, n)] |>
-#                                   lapply(nb_of_stop_codons)))
-# 
-# 
-# plot(xx2)
-# plot(xx2$n, xx2$median)
-# 
+
